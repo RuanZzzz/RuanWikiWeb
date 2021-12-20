ca865796521a884a3817e406abf826857dbab2de
RuanZzzz <734162396@qq.com>
2021-12-20 23:43:07 +0800

    集成Ant Design Vue，并使用按钮测试

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <a-button type="primary" danger>Primary</a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>

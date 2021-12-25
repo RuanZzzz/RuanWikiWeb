c3fc0b9022d9fea87626e35ee87087be69878cb3
RuanZzzz <734162396@qq.com>
2021-12-25 11:00:02 +0800

    配置axios拦截器打印日志

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -98,11 +98,9 @@ export default defineComponent({
 
     // 在onMounted里面就可以写一些初始化的逻辑
     onMounted(() => {
-      console.log("testruan");
       axios.get("/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content
-        console.log(response);
       });
     })
 

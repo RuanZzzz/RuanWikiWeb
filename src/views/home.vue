a7149746d40c099d3620cdfbaf71ff0ae0ffb7e1
RuanZzzz <734162396@qq.com>
2021-12-28 23:35:43 +0800

    修改首页请求参数

--- src/views/home.vue
+++ src/views/home.vue
@@ -77,17 +77,17 @@ import axios from 'axios';
 
 const listData: Record<string, string>[] = [];
 
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'https://www.antdv.com/',
-    title: `ant design vue part ${i}`,
-    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-    description:
-        'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-    content:
-        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
+// for (let i = 0; i < 23; i++) {
+//   listData.push({
+//     href: 'https://www.antdv.com/',
+//     title: `ant design vue part ${i}`,
+//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+//     description:
+//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
+//     content:
+//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+//   });
+// }
 
 export default defineComponent({
   name: 'Home',
@@ -98,9 +98,14 @@ export default defineComponent({
 
     // 在onMounted里面就可以写一些初始化的逻辑
     onMounted(() => {
-      axios.get("/ebook/list").then((response) => {
+      axios.get("/ebook/list",{
+        params: {
+          page : 1,
+          pageSize : 100
+        }
+      }).then((response) => {
         const data = response.data;
-        ebooks.value = data.content
+        ebooks.value = data.content.list
       });
     })
 

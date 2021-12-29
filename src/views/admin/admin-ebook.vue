a969bb00613f2dafae838ee146cf719181ab2008
RuanZzzz <734162396@qq.com>
2021-12-29 21:02:44 +0800

    模态框加入表单，同时编辑显示一行的数据

--- src/views/admin/admin-ebook.vue
+++ src/views/admin/admin-ebook.vue
@@ -19,7 +19,7 @@
         </template>
         <template v-slot:action="{text,record}">
           <a-space size="small">
-            <a-button type="primary" @click="edit">
+            <a-button type="primary" @click="edit(record)">
               编辑
             </a-button>
             <a-button type="danger">
@@ -38,7 +38,24 @@
       :confirm-loading="modalLoading"
       @ok="handleModalOk"
   >
-    <p>test</p>
+    <a-form :model="ebook" :label-col="{ span: 6 }" >
+      <a-form-item label="封面">
+        <a-input v-model:value="ebook.cover" />
+      </a-form-item>
+      <a-form-item label="名称">
+        <a-input v-model:value="ebook.name" />
+      </a-form-item>
+      <a-form-item label="分类一">
+        <a-input v-model:value="ebook.category1Id" />
+      </a-form-item>
+      <a-form-item label="分类二">
+        <a-input v-model:value="ebook.category2Id" />
+      </a-form-item>
+      <a-form-item label="描述">
+        <a-input v-model:value="ebook.desc" type="text"/>
+      </a-form-item>
+    </a-form>
+
   </a-modal>
 </template>
 
@@ -130,6 +147,7 @@
       };
 
       // 表单
+      const ebook = ref({});
       const modalVisible = ref(false);
       const modalLoading = ref(false);
       const handleModalOk = () => {
@@ -141,8 +159,9 @@
       };
 
       // 编辑按钮
-      const edit = () => {
+      const edit = (record: any) => {
         modalVisible.value = true;
+        ebook.value = record;
       }
 
       onMounted(() => {
@@ -159,6 +178,7 @@
         loading,
         handleTableChange, // 因为html需要，所以需要返回
         edit,
+        ebook,
         modalVisible,
         modalLoading,
         handleModalOk

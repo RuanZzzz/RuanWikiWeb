92ab7372d605639af2be05ee28a5701b5408b345
RuanZzzz <734162396@qq.com>
2022-01-12 22:53:58 +0800

    首页显示分类菜单

--- src/views/home.vue
+++ src/views/home.vue
@@ -5,42 +5,21 @@
           v-model:openKeys="openKeys"
           mode="inline"
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
-                <span>
-                  <user-outlined />
-                  subnav 1123
-                </span>
+        <a-menu-item key="welcome">
+          <router-link to="'/'">
+            <MailOutlined/>
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in cateTree" :key="item.id">
+          <template v-slot:title>
+            <span><user-outlined />{{item.name}}</span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-                <span>
-                  <laptop-outlined />
-                  subnav 2
-                </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-                <span>
-                  <notification-outlined />
-                  subnav 3
-                </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <MailOutLined /><span>{{child.name}}</span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -74,20 +53,10 @@
 <script lang="ts">
 import { defineComponent,onMounted,ref } from 'vue';
 import axios from 'axios';
+import {message} from "ant-design-vue";
+import {Tool} from "@/util/tool";
 
 const listData: Record<string, string>[] = [];
-
-// for (let i = 0; i < 23; i++) {
-//   listData.push({
-//     href: 'https://www.antdv.com/',
-//     title: `ant design vue part ${i}`,
-//     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-//     description:
-//         'Ant Design, a design language for background applications, is refined by Ant UED Team.',
-//     content:
-//         'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-//   });
-// }
 
 export default defineComponent({
   name: 'Home',
@@ -96,8 +65,30 @@ export default defineComponent({
     // 让ebooks变成一个响应式的数据
     const ebooks = ref()
 
+    const cateTree = ref();
+    let categorys:any;
+
+    const handleQueryCategory = () => {
+      axios.get("/category/all").then((response) => {
+        const data = response.data;
+        if (data.success) {
+          categorys = data.content;
+
+          cateTree.value = [];
+          cateTree.value = Tool.array2Tree(categorys,0);
+        }else {
+          message.error(data.message);
+        }
+      })
+    };
+
+    const handleClick = () => {
+      console.log("menu click")
+    };
+
     // 在onMounted里面就可以写一些初始化的逻辑
     onMounted(() => {
+      handleQueryCategory();
       axios.get("/ebook/list",{
         params: {
           page : 1,
@@ -124,6 +115,9 @@ export default defineComponent({
         { type: 'LikeOutlined', text: '156' },
         { type: 'MessageOutlined', text: '2' },
       ],
+
+      handleClick,
+      cateTree
     }
   }
 });

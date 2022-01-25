c24023ff97e460d70d380930e211a7191ac5762d
RuanZzzz <734162396@qq.com>
2022-01-25 21:33:43 +0800

    使用树形组件展示文档树（接下来需要通过条件筛选，目前先确定布局）

--- src/views/doc.vue
+++ src/views/doc.vue
@@ -1,9 +1,68 @@
 <template>
   <a-layout>
     <a-layout-content :style="{background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}">
-      <div class="doc">
-        <h1>欢迎来到文档页面</h1>
-      </div>
+      <a-row>
+        <a-col :span="6">
+          <a-tree
+            v-if="docTree.length > 0"
+            :tree-data="docTree"
+            @select="onSelect"
+            :fieldNames="{title: 'name', key: 'id', value: 'id'}"
+            :defaultExpandAll="true"
+          >
+          </a-tree>
+        </a-col>
+        <a-col :span="18">
+
+        </a-col>
+      </a-row>
     </a-layout-content>
   </a-layout>
 </template>
+
+<script lang="ts">
+  import {defineComponent, onMounted, ref, createVNode} from "vue";
+  import axios from "axios";
+  import { message,Modal } from "ant-design-vue";
+  import {Tool} from "@/util/tool";
+  import {useRoute} from "vue-router";
+
+  export default defineComponent({
+    name: 'Doc',
+    setup() {
+      const route = useRoute();
+      const docs = ref();
+      const docTree = ref();
+      docTree.value = [];
+
+      /**
+       * 数据查询
+       */
+      const handleQuery = () => {
+        axios.get("/doc/all").then((response) => {
+          const data = response.data;
+          if (data.success) {
+            docs.value = data.content;
+
+            docTree.value = [];
+            docTree.value = Tool.array2Tree(docs.value,0);
+          }else {
+            message.error(data.message);
+          }
+        })
+      };
+
+      onMounted(() => {
+        handleQuery();
+      });
+
+      return {
+        docTree
+      }
+
+    }
+
+
+  })
+
+</script>

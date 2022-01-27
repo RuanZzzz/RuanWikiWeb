d975cdfaffbc10f92d09155375540c421d8e92fe
RuanZzzz <734162396@qq.com>
2022-01-27 22:36:29 +0800

    完善新增文档

--- src/views/admin/admin-doc.vue
+++ src/views/admin/admin-doc.vue
@@ -196,7 +196,9 @@ export default defineComponent({
     // 表单
     //const doc = ref({});
     const doc = ref();
-    doc.value = {};
+    doc.value = {
+      ebookId: route.query.ebookId
+    };
     const modalVisible = ref(false);
     const modalLoading = ref(false);
 

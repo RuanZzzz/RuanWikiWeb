350d7aa52a48c4427860320bf541581c69417b86
RuanZzzz <734162396@qq.com>
2022-02-13 16:03:43 +0800

    使用vuex保存用户信息

--- src/components/the-footer.vue
+++ src/components/the-footer.vue
@@ -1,13 +1,21 @@
 <template>
   <a-layout-footer style="text-align: center;height: 100px;width: 100%;position: absolute;bottom: 0;left: 0">
-    软烧香的Wiki
+    软烧香的Wiki{{ user.name ? "，欢迎：" + user.name : ""}}
   </a-layout-footer>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import {computed, defineComponent} from 'vue';
+import store from "@/store";
 
 export default defineComponent({
-  name: 'the-footer'
+  name: 'the-footer',
+  setup() {
+    const user = computed(() => store.state.user);
+
+    return {
+      user
+    }
+  }
 });
 </script>

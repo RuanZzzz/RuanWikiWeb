8c64718dd959e0e8d3bdeb352354ab78614452aa
RuanZzzz <734162396@qq.com>
2022-02-16 20:33:06 +0800

    完成文档点赞功能

--- src/views/doc.vue
+++ src/views/doc.vue
@@ -24,6 +24,11 @@
             <a-divider style="height: 2px; background-color: #9999cc" />
           </div>
           <div class="wangeditor" :innerHTML="html"></div>
+          <div class="vote-div">
+            <a-button type="primary" shape="round" :size="'large'" @click="vote">
+              <template #icon><LikeOutlined /> &nbsp; 点赞：{{doc.voteCount}}</template>
+            </a-button>
+          </div>
         </a-col>
       </a-row>
     </a-layout-content>
@@ -99,6 +104,18 @@
         }
       }
 
+      // 点赞
+      const vote = () => {
+        axios.get('/doc/vote/' + doc.value.id).then((response) => {
+          const data = response.data;
+          if (data.success) {
+            doc.value.voteCount++;
+          }else {
+            message.error(data.message);
+          }
+        })
+      }
+
       onMounted(() => {
         handleQuery();
       });
@@ -108,7 +125,8 @@
         onSelect,
         html,
         defaultSelectedKeys,
-        doc
+        doc,
+        vote
       }
 
     }
@@ -171,4 +189,11 @@
   font-size: 16px !important;
   font-weight: 600;
 }
+
+/*点赞*/
+.vote-div {
+  padding: 15px;
+  text-align: center;
+}
+
 </style>
